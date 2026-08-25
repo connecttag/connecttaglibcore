241544dcaf86f075bb4059f51cd82db0ed8c2c00
‪Aqel Mohammed‬‏ <aqelmoh77@gmail.com>
2026-08-25 08:20:07 +0300

    update 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     buildFeatures {
         compose = true
     }
